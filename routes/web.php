e700b014a0a1b674900a48f61f63f5a8f515bb6b
Alexiotovv <gpsalexvasquez@gmail.com>
2025-11-07 08:25:09 -0500

    gestion

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::post('/marcar', [AsistenciaController::class, 'marcarAsistencia'])->name(
 // Auth::routes(['verify' => false]);
 Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
-Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
 
 // PROTEGIDAS
 Route::middleware(['auth'])->group(function () {
